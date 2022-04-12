f02bce84e30d51642fdd785960d5775fc9bb3353
JWilson1431 <Jwilson1431@gmail.com>
2022-04-12 10:05:34 -0400

    first commit-H202 Page created

---
+++ app/src/main/java/UI/H202Page.java
@@ -0,0 +1,68 @@
+package UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.vetnurse.R;
+
+public class H202Page extends AppCompatActivity {
+    EditText enterDogLbs;
+    int dogLbs;
+    TextView dosageText;
+    String dosageResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_h202_page);
+    }
+
+    public void calculateH202(View view) {
+        enterDogLbs=findViewById(R.id.enterDogLbs);
+        dogLbs= Integer.parseInt(enterDogLbs.getText().toString());
+        dosageText =findViewById(R.id.dosageResult);
+        dosageResult=dosageText.getText().toString();
+
+
+        if(dogLbs <0){
+            dosageResult="This is not an accurate weight, please try again";
+        }
+
+        else if(dogLbs >0 && dogLbs<= 5){
+            dosageResult="Give 1 teaspoon of 3% hydrogen peroxide. Dosage can be repeated in 15 minutes if the dog does not vomit";
+        }
+
+        else if(dogLbs >=6 && dogLbs <=10){
+            dosageResult="Give 2 teaspoons of 3% hydrogen peroxide. Dosage can be repeated in 15 minutes if the dog does not vomit";
+        }
+
+        else if(dogLbs >=11 && dogLbs <=15){
+            dosageResult="Give 1 tablespoon of 3% hydrogen peroxide. Dosage can be repeated in 15 minutes if the dog does not vomit";
+        }
+
+        else if (dogLbs >=16 && dogLbs <=20){
+            dosageResult="Give 1 tablespoon and 1 teaspoon of 3% hydrogen peroxide. Dosage can be repeated in 15 minutes if the dog does not vomit";
+        }
+
+        else if(dogLbs >=21 && dogLbs <=25){
+            dosageResult="Give 1 & 1/2 tablespoons of 3% hydrogen peroxide. Dosage can be repeated in 15 minutes if the dog does not vomit.";
+        }
+
+        else if (dogLbs >=26 && dogLbs <=30){
+            dosageResult="Give 2 tablespoons of 3% hydrogen peroxide. Dosage can be repeated in 15 minutes if the dog does not vomit";
+        }
+
+        else if(dogLbs >=31 && dogLbs <=40){
+            dosageResult="Give 2 & 1/2 tablespoons of 3% hydrogen peroxide. Dosage can be repeated in 15 minutes if the dog does not vomit;";
+        }
+
+        else if(dogLbs >41){
+            dosageResult="Give 3 tablespoons of 3% hydrogen peroxide. Dosage can be repeated in 15 minutes if the dog does not vomit";
+        }
+
+    }
+}
---
+++ app/src/main/java/UI/MainActivity.java
@@ -0,0 +1,23 @@
+package UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.vetnurse.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void H202Page(View view) {
+        Intent intent= new Intent(MainActivity.this, H202Page.class);
+        startActivity(intent);
+    }
+}
